12b5b6015fade6ef107674e6b900d0cafa3f6bb0
Galyshka <volkrokvolk@gmail.com>
2026-05-28 12:52:11 +0300

    Initial Commit

---
+++ ImagesSearchCropper/ImagesSearchCropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesSearchCropperApp.swift
+//  ImagesSearchCropper
+//
+//  Created by Володимир Галушка on 28.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct ImagesSearchCropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
